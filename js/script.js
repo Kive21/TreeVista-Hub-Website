cf41f6618e091ff65e7ac64d934c9f237b1330f5
Eric Kiverenge <avwengae@gmail.com>
2024-06-15 19:51:44 +0300

    Added media querries for responsiveness

--- js/script.js
+++ js/script.js
@@ -1,43 +1,48 @@
-// Toggle mobile menu
-const menuIcon = document.querySelector('.menu-icon');
-const menuItems = document.getElementById('MenuItems');
-
-menuIcon.addEventListener('click', () => {
-  menuItems.classList.toggle('show');
-});
-
-// Smooth scrolling
-const links = document.querySelectorAll('a[href^="#"]');
-
-links.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const targetId = event.target.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+document.addEventListener('DOMContentLoaded', () => {
+    // Toggle mobile menu
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuItems = document.getElementById('MenuItems');
+  
+    function toggleMenu() {
+      menuItems.classList.toggle('show');
+    }
+  
+    menuIcon.addEventListener('click', toggleMenu);
+  
+    // Smooth scrolling
+    const links = document.querySelectorAll('a[href^="#"]');
+  
+    links.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  
+    // Sticky header
+    const header = document.querySelector('.header');
+    const heroSection = document.querySelector('.row');
+  
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > heroSection.offsetTop){
+        header.classList.add('sticky');
+      } else {
+        header.classList.remove('sticky');
+      }
+    });
+  
+    // Product hover animation
+    const products = document.querySelectorAll('.col-4');
+  
+    products.forEach((product) => {
+      product.addEventListener('mouseenter', () => {
+        product.classList.add('hover');
+      });
+      product.addEventListener('mouseleave', () => {
+        product.classList.remove('hover');
+      });
+    });
   });
-});
-
-// Sticky header
-const header = document.querySelector('.header');
-const heroSection = document.querySelector('.row');
-
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > heroSection.offsetTop) {
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky');
-  }
-});
-
-// Product hover animation
-const products = document.querySelectorAll('.col-4');
-
-products.forEach((product) => {
-  product.addEventListener('mouseenter', () => {
-    product.classList.add('hover');
-  });
-  product.addEventListener('mouseleave', () => {
-    product.classList.remove('hover');
-  });
-});
+  
